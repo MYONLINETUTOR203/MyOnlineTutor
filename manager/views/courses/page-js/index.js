e4e635193d666dae8fb9e84a38fa5e0b5506cfa9
iqbal <iqbal.kaur@fatbit.in>
2022-07-15 09:58:35 +0530

    task-99792:fixed a paging issue

--- manager/views/courses/page-js/index.js
+++ manager/views/courses/page-js/index.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
     var dv = '#listing';
     goToSearchPage = function (pageno) {
         var frm = document.frmPaging;
-        $(frm.pageno).val(pageno);
+        $(frm.page).val(pageno);
         search(frm);
     };
     search = function (form) {
