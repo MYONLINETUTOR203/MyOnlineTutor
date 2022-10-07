1258fdeff643cafcdd56a75cbdad16db5eb6b038
Sher Singh <sher.singh@fatbit.in>
2022-10-07 12:04:37 +0530

    task-98646 Setting update admin logo info message

--- manager/views/configurations/form.php
+++ manager/views/configurations/form.php
@@ -38,7 +38,7 @@ switch ($frmType) {
         $applyToTeachImage = $frm->getField('apply_to_teach_banner');
         if ($canEdit) {
             $adminLogoFld->htmlAfterField = sprintf(Label::getLabel('LBL_Dimensions_%s'), '200*100');
-            $desktopLogoFld->htmlAfterField = sprintf(Label::getLabel('LBL_Dimensions_%s'), '200*100');
+            $desktopLogoFld->htmlAfterField = str_replace(['{width}', '{height}'], ['200', '67'], Label::getLabel('LBL_For_best_view_width_{width}px_and_height_{height}px'));
             $emailLogoFld->htmlAfterField = sprintf(Label::getLabel('LBL_Dimensions_%s'), '200*100');
             $faviconFld->htmlAfterField = str_replace(['{dimensions}', '{ext}'], ['16*16', '.ico'], Label::getLabel('LBL_FAV_DIMENSIONS_{dimensions}_AND_EXTENSION_{ext}'));
             $blogImg->htmlAfterField = sprintf(Label::getLabel('LBL_Dimensions_%s'), '1600*480');
@@ -146,6 +146,6 @@ switch ($frmType) {
     </ul>
 <?php } ?>
 <div class="tabs_panel_wrap">
-<?php echo $frm->getFormHtml(); ?>
+    <?php echo $frm->getFormHtml(); ?>
 
 </div>
