dbd835800abce7069106fd70222a3135664da8ae
Dharminder Singh <dharminder@ablysoft.com>
2022-11-09 18:08:55 +0530

    task-102494 made CSS for OTP screen

--- application/views/guest-user/two-factor-form.php
+++ application/views/guest-user/two-factor-form.php
@@ -1,6 +1,6 @@
 <?php
 defined('SYSTEM_INIT') or die('Invalid Usage.');
-$frm->setFormTagAttribute('class', 'form two-factor-form digit-group');
+$frm->setFormTagAttribute('class', 'form two-factor-form');
 $frm->setFormTagAttribute('action', '');
 $frm->setFormTagAttribute('onSubmit', 'setupTwoFactor(this); return(false);');
 $fld1= $frm->getField('digit_1');
@@ -34,63 +34,45 @@ $btn_submit = $frm->getField('btn_submit');
 $btn_submit->setFieldTagAttribute('id', 'btn_submit');
 $btn_submit->setFieldTagAttribute('disabled', 'disabled');
 ?>
-<style>
-    .digit-group input{
-		width: 30px;
-		height: 50px;
-        padding: 2px;
-		border: 1px solid #000;
-		line-height: 50px;
-		text-align: center;
-		font-size: 24px;
-		font-weight: 200;
-		color: #000;
-		margin: 0 2px;
-	}
 
-</style>
-<section class="section section--gray section--page">
-    <div class="container container--fixed">
-        <div class="row justify-content-center">
-            <div class="col-12">
-                <div class="box -skin">
-                    <div class="box__head -align-center">
-                        <h4 class="-border-title"><?php echo Label::getLabel('LBL_LOGIN'); ?></h4>
-                    </div>
-                    <div class="box__body -padding-40 div-login-form">
-                        <div class="prompt">
-                            Enter the code generated on your mobile device below to log in!
-                        </div>
-                        <?php
-                            echo $frm->getFormTag(); 
-                            echo $frm->getFieldHtml('digit_1'); 
-                            echo $frm->getFieldHtml('digit_2'); 
-                            echo $frm->getFieldHtml('digit_3'); 
-                            echo $frm->getFieldHtml('digit_4'); 
-                            echo $frm->getFieldHtml('digit_5'); 
-                            echo $frm->getFieldHtml('digit_6'); 
-                            echo $frm->getFieldHtml('user_id'); 
-                        ?>
-                            <span class="-gap"></span><span class="-gap"></span>
-                        
-                            <div class="-align-center">
-                                <div>
-                                    <?php echo $frm->getFieldHtml('btn_submit'); ?>
-                                </div>
-                                <div>
-                                    <?php echo $frm->getFieldHtml('resend_auth_code'); ?>
-                                </div>
-                            </div>
-                        </form>
-                        <?php 
-                         echo $frm->getExternalJs();
-                        ?>
-                    </div>
-                </div>
-            </div>
+<div class="box box--narrow">
+    
+    <div class="auth-screen">
+        <hgroup>
+            <h4 class="-border-title margin-bottom-2"><?php echo Label::getLabel('LBL_Verification_Code'); ?></h4>
+            <p>Please Enter the OTP sent on your mobile number.</p>
+        </hgroup>
+        
+        <?php
+            echo $frm->getFormTag(); 
+        ?>
+        <div class=" digit-group">
+        <?php
+            echo $frm->getFieldHtml('digit_1'); 
+            echo $frm->getFieldHtml('digit_2'); 
+            echo $frm->getFieldHtml('digit_3'); 
+            echo $frm->getFieldHtml('digit_4'); 
+            echo $frm->getFieldHtml('digit_5'); 
+            echo $frm->getFieldHtml('digit_6'); 
+            echo $frm->getFieldHtml('user_id'); 
+        ?>
         </div>
+           
+            
+                <div class="margin-bottom-4">
+                    <?php echo $frm->getFieldHtml('btn_submit'); ?>
+                </div>
+
+                <div class="auth-msg">
+                    <?php echo $frm->getFieldHtml('resend_auth_code'); ?>
+                </div>
+           
+        </form>
+        <?php 
+            echo $frm->getExternalJs();
+        ?>
     </div>
-</section>
+</div>
 <script>
     var uid = '<?php echo $userFld->value; ?>';
 
