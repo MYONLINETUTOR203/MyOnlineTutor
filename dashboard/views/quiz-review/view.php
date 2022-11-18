7adc008dbe1a73ecec6002639057a2186b33fa62
iqbal <iqbal.kaur@fatbit.in>
2022-11-18 15:57:08 +0530

    bug-068484 - Show NA if the manual question answer is not provided by the learner

--- dashboard/views/quiz-review/view.php
+++ dashboard/views/quiz-review/view.php
@@ -55,7 +55,7 @@
                     <div class="answer-view margin-bottom-10">
                         <p>
                             <strong><?php echo Label::getLabel('LBL_ANSWER:') ?></strong>
-                            <?php echo $answers ?>
+                            <?php echo !empty($answers) ? $answers : Label::getLabel('LBL_NA') ?>
                         </p>
                         <?php if ($siteUserType == User::TEACHER) { ?>
                             <div class="margin-top-10">
