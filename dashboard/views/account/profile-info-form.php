e3977bdda39501b926c0ff2a45cef51bf5809c01
iqbal <iqbal.kaur@fatbit.in>
2022-12-06 11:13:29 +0530

    bug-069045 - Help text should come under the 2-factor authentication setting

--- dashboard/views/account/profile-info-form.php
+++ dashboard/views/account/profile-info-form.php
@@ -61,9 +61,9 @@ if (MyUtility::getLayoutDirection() == 'rtl') {
                     <?php
                     if ($siteUserType == User::TEACHER) {
                         foreach ($languages as $langId => $language) {
-                            ?>
+                    ?>
                             <li class="profile-lang-tab"><a href="javascript:void(0);" class="profile-lang-li" onclick="getLangProfileInfoForm(<?php echo $langId; ?>);"><?php echo $language['language_name']; ?></a></li>
-                            <?php
+                    <?php
                         }
                     }
                     ?>
@@ -98,17 +98,17 @@ if (MyUtility::getLayoutDirection() == 'rtl') {
                             <div class="col-sm-auto">
                                 <a onclick="googleCalendarAuthorize();" href="javascript:void(0);" class="social-button social-button--google">
                                     <span class="social-button__media">
-                                    <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                        <g transform="translate(-187 -241)">
-                                            <rect  width="24" height="24" transform="translate(187 241)" fill="none"/>
-                                            <g transform="translate(190 243)">
-                                            <path  d="M4.211,144.619l-.661,2.469-2.417.051a9.517,9.517,0,0,1-.07-8.871h0l2.152.395.943,2.139a5.67,5.67,0,0,0,.053,3.817Z" transform="translate(0 -133.137)" fill="#fbbb00"/>
-                                            <path d="M270.753,208.176a9.5,9.5,0,0,1-3.387,9.183h0l-2.711-.138-.384-2.395a5.662,5.662,0,0,0,2.436-2.891h-5.08v-3.758h9.125Z" transform="translate(-251.919 -200.451)" fill="#518ef8"/>
-                                            <path  d="M44.824,314.835h0a9.5,9.5,0,0,1-14.315-2.906l3.079-2.52a5.65,5.65,0,0,0,8.142,2.893Z" transform="translate(-29.377 -297.927)" fill="#28b446"/>
-                                            <path d="M43.126,2.187l-3.078,2.52a5.649,5.649,0,0,0-8.329,2.958L28.625,5.131h0a9.5,9.5,0,0,1,14.5-2.944Z" transform="translate(-27.562)" fill="#f14336"/>
+                                        <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                            <g transform="translate(-187 -241)">
+                                                <rect width="24" height="24" transform="translate(187 241)" fill="none" />
+                                                <g transform="translate(190 243)">
+                                                    <path d="M4.211,144.619l-.661,2.469-2.417.051a9.517,9.517,0,0,1-.07-8.871h0l2.152.395.943,2.139a5.67,5.67,0,0,0,.053,3.817Z" transform="translate(0 -133.137)" fill="#fbbb00" />
+                                                    <path d="M270.753,208.176a9.5,9.5,0,0,1-3.387,9.183h0l-2.711-.138-.384-2.395a5.662,5.662,0,0,0,2.436-2.891h-5.08v-3.758h9.125Z" transform="translate(-251.919 -200.451)" fill="#518ef8" />
+                                                    <path d="M44.824,314.835h0a9.5,9.5,0,0,1-14.315-2.906l3.079-2.52a5.65,5.65,0,0,0,8.142,2.893Z" transform="translate(-29.377 -297.927)" fill="#28b446" />
+                                                    <path d="M43.126,2.187l-3.078,2.52a5.649,5.649,0,0,0-8.329,2.958L28.625,5.131h0a9.5,9.5,0,0,1,14.5-2.944Z" transform="translate(-27.562)" fill="#f14336" />
+                                                </g>
                                             </g>
-                                        </g>
-                                    </svg>
+                                        </svg>
                                     </span>
                                     <span class="social-button__label"><?php echo Label::getLabel('LBL_CONNECT_GOOGLE_CALENDAR'); ?></span>
                                 </a>
@@ -305,7 +305,7 @@ if (MyUtility::getLayoutDirection() == 'rtl') {
                                                         <span class="switch-group__label free-trial-status-js"><?php echo ($freeTrialField->checked) ? Label::getLabel('LBL_Active') : Label::getLabel('LBL_In-active'); ?></span>
                                                         <span class="switch switch--small">
                                                             <input class="switch__label" type="<?php echo $freeTrialField->fldType; ?>" name="<?php echo $freeTrialField->getName(); ?>" value="<?php echo $freeTrialField->value; ?>" <?php echo ($freeTrialField->checked) ? 'checked' : ''; ?>>
-                                                                <i class="switch__handle bg-green"></i>
+                                                            <i class="switch__handle bg-green"></i>
                                                         </span>
                                                     </label>
                                                 </div>
@@ -331,9 +331,12 @@ if (MyUtility::getLayoutDirection() == 'rtl') {
                                                         <span class="switch-group__label user-2fa-status-js"><?php echo ($user2faField->checked) ? Label::getLabel('LBL_Active') : Label::getLabel('LBL_In-active'); ?></span>
                                                         <span class="switch switch--small">
                                                             <input class="switch__label" type="<?php echo $user2faField->fldType; ?>" name="<?php echo $user2faField->getName(); ?>" value="<?php echo $user2faField->value; ?>" <?php echo ($user2faField->checked) ? 'checked' : ''; ?>>
-                                                                <i class="switch__handle bg-green"></i>
+                                                            <i class="switch__handle bg-green"></i>
                                                         </span>
                                                     </label>
+                                                    <small>
+                                                        <?php echo Label::getLabel('LBL_TWO_FACTOR_SETTING_IS_NOT_APPLICABLE_FOR_SOCIAL_MEDIA_LOGINS'); ?>
+                                                    </small>
                                                 </div>
                                             </div>
                                         </div>
@@ -367,34 +370,36 @@ if (MyUtility::getLayoutDirection() == 'rtl') {
     var statusInActive = '<?php echo Label::getLabel('LBL_In-active'); ?>';
 
 
-    $(document).ready(function () {
+    $(document).ready(function() {
         $("[name='user_timezone'], [name='user_country_id'], [name='user_phone_code']").select2();
-        $("[name='user_country_id']").on('change', function () {
+        $("[name='user_country_id']").on('change', function() {
             $("[name='user_phone_code']").val($("[name='user_country_id']").val());
-            $("[name='user_phone_code']").css({width: '100%'});
+            $("[name='user_phone_code']").css({
+                width: '100%'
+            });
             $("[name='user_phone_code']").next().remove();
             $("[name='user_phone_code']").select2();
         });
-        $('input[name="user_username"]').on('keypress', function (e) {
+        $('input[name="user_username"]').on('keypress', function(e) {
             if (e.which == 32) {
                 return false;
             }
         });
-        $('input[name="user_username"]').on('change', function (e) {
+        $('input[name="user_username"]').on('change', function(e) {
             var user_name = $(this).val();
             user_name = user_name.replace(/ /g, "");
             $(this).val(user_name);
             $('.user_username_span').html(user_name);
         });
-        $('input[name="user_username"]').on('keyup', function () {
+        $('input[name="user_username"]').on('keyup', function() {
             var user_name = $(this).val();
             $('.user_username_span').html(user_name);
         });
-        $('input[name="user_trial_enabled"]').on('change', function () {
+        $('input[name="user_trial_enabled"]').on('change', function() {
             let status = ($(this).is(':checked')) ? statusActive : statusInActive;
             $('.free-trial-status-js').text(status);
         });
-        $('input[name="user_2fa_enabled"]').on('change', function () {
+        $('input[name="user_2fa_enabled"]').on('change', function() {
             let status = ($(this).is(':checked')) ? statusActive : statusInActive;
             $('.user-2fa-status-js').text(status);
         });
