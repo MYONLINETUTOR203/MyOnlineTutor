b5c791c89c578b3092f344905fcb0f6a4cbd5eb2
Sher Singh <sher.singh@fatbit.in>
2022-12-27 12:48:53 +0530

    task-101808 Updates request demo popup for demo

--- restore/view/header-bar.php
+++ restore/view/header-bar.php
@@ -5,7 +5,6 @@
         right: 1rem;
         z-index: 9999;
     }
-
     .-fixed-wrap a {
         position: relative;
         display: inline-block;
@@ -28,7 +27,6 @@
         overflow: hidden;
         min-width: 150px;
     }
-
     .-fixed-wrap a small {
         position: absolute;
         top: 0;
@@ -41,25 +39,21 @@
         white-space: nowrap;
         background-color: rgba(0, 0, 0, 0.2);
     }
-
     .restore-demo-bg {
         background-image: url('<?php echo MyUtility::makeFullUrl('', '', array(), CONF_WEBROOT_FRONT_URL) . 'images/catalog-bg.png'; ?>') !important;
         background-color: #fff !important;
         background-repeat: no-repeat !important;
         background-position: 130% top !important;
     }
-
     .restore-demo .demo-data-inner>ul,
     .restore-demo .demo-data-inner .heading {
         max-width: 500px;
         margin-right: 250px;
     }
-
     .demo-data-inner {
         margin: 20px;
         color: #4c4c4c;
     }
-
     .demo-data-inner .heading {
         font-size: 4rem;
         font-weight: 600;
@@ -69,7 +63,6 @@
         margin-bottom: 40px;
         color: inherit;
     }
-
     .demo-data-inner .heading:after {
         background: var(--color-primary);
         width: 60px;
@@ -79,13 +72,11 @@
         content: "";
         display: block;
     }
-
     .demo-data-inner .heading span {
         display: block;
         font-size: 0.8rem;
         text-transform: none;
     }
-
     .demo-data-inner ul li {
         position: relative;
         margin: 10px 0;
@@ -93,7 +84,6 @@
         display: block;
         font-size: 0.9rem;
     }
-
     .demo-data-inner ul li:before {
         width: 5px;
         height: 5px;
@@ -105,31 +95,25 @@
         transform: rotate(45deg);
         background: #4c4c4c;
     }
-
     .demo-data-inner ul ul {
         margin-inline-start: 15px;
         margin-bottom: 20px;
     }
-
     .restore-demo {
         min-height: 300px;
     }
-
     .restore-demo a {
         color: var(--secondary-color);
     }
-
     .restore-demo p {
         font-size: 1.1rem;
         font-weight: 400;
         line-height: 1.5;
     }
-
     #facebox .restore-demo.fbminwidth {
         min-width: 350px;
         min-height: 150px;
     }
-
     #facebox .restore-demo {
         display: block;
         width: 100%;
@@ -139,7 +123,6 @@
         margin: 0 auto;
         position: relative;
     }
-
     .demo-data-inner ul li {
         position: relative;
         margin: 10px 0;
@@ -148,8 +131,6 @@
         font-size: 0.9rem;
         line-height: 1.5;
     }
-
-    /* preview header */
     .preview-header{
         background-color:#000;
         color: #fff;
@@ -173,7 +154,6 @@
         font-weight: 800;
         color: #ff793d;
     }
-
     .preview-container{
         min-height: 40px;
         display: -webkit-box;
@@ -186,9 +166,7 @@
         -ms-flex-pack: justify;
         justify-content: space-between;
     }
-
     @media(min-width:576px){
-        /* .is-preview-on .header.nav-up,.is-preview-on .header.nav-down{transform: translateY(inherit);-webkit-transform: translateY(inherit);} */
         .is-preview-on .header.nav-down, .is-preview-on #header{
             top: 60px;
         }
@@ -199,38 +177,15 @@
             top: 8.6rem;
             transition: none;
         }
-
-
-
-
-        /* .is-tutor-listing.is-preview-on .header, .is-group-classes.is-preview-on .header, .teachers.teachers-index.is-preview-on .header, .groupclasses.groupclasses-index.is-preview-on .header, .teachers.teachers-languages.is-preview-on .header
-         {position: sticky;} */
-
-        /* .is-preview-on .header .header-primary{box-shadow: none;} */
-
     }
-
-    /* .is-preview-on #header, .is-preview-on.is-filter-fixed #header{top: 60px;} */
     .is-preview-on .leftside{
         padding-top: 132px;
     }
-
     @media(min-width:1199px){
-        /* .is-preview-on.is-filter-fixed .header{top: 60px;} */
         .is-preview-on.is-filter-fixed .section-filters{
             top:60px;
         }
     }
-
-
-
-
-    /* @media(min-width:1199px){
-        .preview-container__cell{-webkit-box-flex: 1;-ms-flex: 1;flex: 1;}  
-
-       
-    } */
-
     .preview-container__cell{
         display: -webkit-box;
         display: -ms-flexbox;
@@ -239,14 +194,11 @@
         -ms-flex-align: center;
         align-items: center;
     }
-
     .preview-progress{
         min-width: 240px;
         max-width: 240px;
         padding: 0.4rem 1rem;
     }
-
-
     .preview-controls{
         position: relative;
         margin: 0 auto;
@@ -287,7 +239,6 @@
     .preview-controls__action:hover .control-svg{
         opacity: 1;
     }
-
     .control-svg{
         width: 32px;
         height: 32px;
@@ -301,22 +252,15 @@
         overflow: hidden;
         fill: currentColor;
     }
-
-    .preview-button{
-        display: inline-flex;
+    .btn-preview{
         padding: 0.7rem 1.2rem;
-        background-color: #ff5317;
         border: 1px solid transparent;
         font-size: 13px;
         border-radius:3px;
     }
-    .preview-button:hover{
-        background-color:#0037B4;
-    }
     .buttons-bucket{
         margin-left: auto;
     }
-
     .preview-close{
         width: 60px;
         height: 60px;
@@ -339,7 +283,6 @@
         transform: rotate(45deg);
         -webkit-transform: rotate(45deg);
     }
-
     .progress-ui {
         width: 100%;
         height: 0.3rem;
@@ -366,25 +309,20 @@
         height: 100%;
         font-size: 11px;
     }
-
-
     .preview-controls__action--admin .control-svg{
         width: 30px;
         height: 30px;
     }
-
     @media(max-width:1199px){
         .preview-container__cell.preview-container__middle{
             display: none;
         }
     }
-
     @media(max-width:767px){
         .preview-header{
             display: none;
         }
     }
-
     body[dir='rtl'] .buttons-bucket{
         margin-right: auto;
         margin-left: 0;
@@ -393,16 +331,12 @@
         margin-left: 0;
         margin-right: 1rem;
     }
-
-
-
 </style>
 <?php
 $backendUrl = MyUtility::makeFullUrl('', '', [], CONF_WEBROOT_BACKEND);
 $frontendUrl = MyUtility::makeFullUrl('', '', [], CONF_WEBROOT_FRONTEND);
 $mobileUrl = MyUtility::makeFullUrl('Mobile', '', [], CONF_WEBROOT_FRONTEND);
 $requestUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-
 $mobileView = '';
 $backendView = '';
 $frontendView = '';
@@ -455,9 +389,9 @@ if (strpos($requestUrl, $mobileUrl) > -1) {
         </div>
         <div class="preview-container__cell preview-container__right">
             <div class="buttons-bucket">
-                <a target="_blank" href="https://www.fatbit.com/online-learning-consultation-marketplace-platform.html" class="preview-button"><?php echo Label::getLabel('LBL_START_YOUR_MARKETPLACE'); ?></a>
-                <a target="_blank" href="https://www.yo-coach.com/?demo_form" class="preview-button"><?php echo Label::getLabel('LBL_Get_A_Personalized_Demo'); ?></a>
-                <!-- <a target="_blank" href="https://www.yo-coach.com/clients.html" class="preview-button"><?php echo Label::getLabel('LBL_OUR_CLIENTS') ?></a> -->
+                <a target="_blank" href="https://www.fatbit.com/online-learning-consultation-marketplace-platform.html" class="btn-preview btn btn--secondary"><?php echo Label::getLabel('LBL_START_YOUR_MARKETPLACE'); ?></a>
+                <a target="_blank" href="https://www.yo-coach.com/?demo_form" class="btn-preview btn btn--secondary"><?php echo Label::getLabel('LBL_Get_A_Personalized_Demo'); ?></a>
+                <!-- <a target="_blank" href="https://www.yo-coach.com/clients.html" class="btn-preview btn btn--secondary"><?php echo Label::getLabel('LBL_OUR_CLIENTS') ?></a> -->
             </div>
             <a href="javascript:void(0)" class="preview-close" onclick="closePreview();"></a>
         </div>
@@ -465,12 +399,14 @@ if (strpos($requestUrl, $mobileUrl) > -1) {
 </div>
 <script>
     function requestDemoPopup(key, seconds) {
-        if ($.cookie(key)) {
+        if ($.cookie(key) != undefined && parseInt($.cookie(key)) < parseInt((new Date()).getTime() / 1000)) {
             return;
         }
-        var showtime = new Date();
-        showtime.setSeconds(showtime.getSeconds() + seconds);
-        $.cookie(key, showtime.getTime() / 1000);
+        if ($.cookie(key) == undefined) {
+            var showtime = new Date();
+            showtime.setSeconds(showtime.getSeconds() + seconds);
+            $.cookie(key, parseInt(showtime.getTime() / 1000));
+        }
         var interval = setInterval(function () {
             var startTime = parseInt($.cookie(key));
             var currentTime = parseInt((new Date()).getTime() / 1000);
@@ -492,18 +428,15 @@ if (strpos($requestUrl, $mobileUrl) > -1) {
         $('.preview-header').slideUp();
         $('body').removeClass('is-preview-on');
     }
-
     function showRestorePopup() {
         $.facebox('<div class="demo-data-inner"><div class="heading">Yo!Coach<span></span></div> <p>To enhance your demo experience, we periodically  restore our database every 24 hours.</p><br> <p>For technical issues :-</p> <ul> <li><strong>Call us at: </strong>+1 469 844 3346, +91 85919 19191, +91 95555 96666, +91 73075 70707, +91 93565 35757</li> <li><strong>Mail us at : </strong> <a href="mailto:sales@fatbit.com">sales@fatbit.com</a></li> </ul> <br> Create Your Online Tutoring & Consultation Platform With Yo!Coach <a href="https://www.fatbit.com/website-design-company/requestaquote.html" target="_blank">Click here</a></li></div>', 'restore-demo restore-demo-bg fbminwidth');
     }
-
     function restoreSystem() {
         fcom.process('Restore is in process..');
         fcom.updateWithAjax(fcom.makeUrl('RestoreSystem', 'index', '', '/'), '', function (resp) {
             window.location.reload();
         }, false, false);
     }
-
     $(document).on("click", "#demoBoxClose", function (e) {
         $('.demo-header').hide();
         $('html').removeClass('sticky-demo-header');
@@ -519,7 +452,6 @@ if (strpos($requestUrl, $mobileUrl) > -1) {
         var now = utcDate.getTime();
         // Find the distance between now and the count down date
         var distance = countDownDate - now - 65000;
-
         // Time calculations for days, hours, minutes and seconds
         // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
