011756cb83e72a1617f8216ccfe6cbc387b42e91
iqbal <iqbal.kaur@fatbit.in>
2022-12-27 17:08:09 +0530

    task-101808 uncommented demo url condition

--- application/utilities/MyUtility.php
+++ application/utilities/MyUtility.php
@@ -609,7 +609,7 @@ class MyUtility extends FatUtility
      */
     public static function isDemoUrl(): bool
     {
-        return true; //(strtolower($_SERVER['SERVER_NAME']) === 'elearning.yo-coach.com');
+        return (strtolower($_SERVER['SERVER_NAME']) === 'elearning.yo-coach.com');
     }
 
     /**
